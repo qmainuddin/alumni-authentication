2005237732f3cd545e7b940e908aa2a68349fbce
Mainuddin Talukdar <mtalukdar@vantage.com>
2023-11-19 15:25:25 -0600

    initial commit

---
+++ src/main/java/org/miu/alumni/alumniauthentication/AlumniAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package org.miu.alumni.alumniauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlumniAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/miu/alumni/alumniauthentication/AlumniAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miu.alumni.alumniauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
