ea978e2272cee632ced8a2e625e0ecd885892023
Mainuddin Talukdar <mtalukdar@vantage.com>
2023-11-21 01:25:01 -0600

    added client dependency for service discovery and config server

--- src/main/java/org/miu/alumni/alumniauthentication/AlumniAuthenticationApplication.java
+++ src/main/java/org/miu/alumni/alumniauthentication/AlumniAuthenticationApplication.java
@@ -5,8 +5,10 @@ import io.swagger.v3.oas.models.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AlumniAuthenticationApplication {
 
